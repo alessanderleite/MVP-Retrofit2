302ab74420e0d105099b764188f751e48a2d0310
alessanderleite <alessander.logistica@gmail.com>
2019-04-10 12:54:54 -0300

    create the interface named MainContract with three inner interfaces inside

---
+++ app/src/main/java/br/com/alessanderleite/mvpretrofit2/main_activity/MainContract.java
@@ -0,0 +1,29 @@
+package br.com.alessanderleite.mvpretrofit2.main_activity;
+
+import java.util.ArrayList;
+
+public interface MainContract {
+
+    interface presenter {
+        void onDestroy();
+        void onRefreshButtonClick();
+        void requestDataFromServer();
+    }
+
+    interface MainView {
+        void showProgress();
+        void hideProgress();
+        void setDataToRecyclerView(ArrayList<Notice> noticeArrayList);
+        void onResponseFailure(Throwable throwable);
+    }
+
+    interface GetNoticeIntractor {
+
+        interface OnFinishedListener {
+            void onFinished(ArrayList<Notice> noticeArrayList);
+            void onFailure(Throwable t);
+        }
+
+        void getNoticeArrayList(OnFinishedListener onFinishedListener);
+    }
+}
