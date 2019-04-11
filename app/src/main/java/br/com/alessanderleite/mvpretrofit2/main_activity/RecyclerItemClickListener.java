45ca9a8b5b396e016819021571790b45540e40fb
alessanderleite <alessander.logistica@gmail.com>
2019-04-11 16:23:31 -0300

    create the interface named RecyclerItemClickListener and add the onItemClick() method inside

---
+++ app/src/main/java/br/com/alessanderleite/mvpretrofit2/main_activity/RecyclerItemClickListener.java
@@ -0,0 +1,7 @@
+package br.com.alessanderleite.mvpretrofit2.main_activity;
+
+import br.com.alessanderleite.mvpretrofit2.model.Notice;
+
+public interface RecyclerItemClickListener {
+    void onItemClick(Notice notice);
+}
