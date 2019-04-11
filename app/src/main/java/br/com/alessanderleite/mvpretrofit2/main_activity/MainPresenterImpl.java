5e491d4f99328499c58890e70812e2ecf22ed7c0
alessanderleite <alessander.logistica@gmail.com>
2019-04-11 16:19:56 -0300

    code the MainPresenterImpl class and implements the methods

--- app/src/main/java/br/com/alessanderleite/mvpretrofit2/main_activity/MainPresenterImpl.java
+++ app/src/main/java/br/com/alessanderleite/mvpretrofit2/main_activity/MainPresenterImpl.java
@@ -1,4 +1,51 @@
 package br.com.alessanderleite.mvpretrofit2.main_activity;
 
-public class MainPresenterImpl {
+import java.util.ArrayList;
+
+import br.com.alessanderleite.mvpretrofit2.model.Notice;
+
+public class MainPresenterImpl implements MainContract.presenter, MainContract.GetNoticeIntractor.OnFinishedListener {
+
+    private MainContract.MainView mainView;
+    private MainContract.GetNoticeIntractor getNoticeIntractor;
+
+    public MainPresenterImpl(MainContract.MainView mainView, MainContract.GetNoticeIntractor getNoticeIntractor) {
+        this.mainView = mainView;
+        this.getNoticeIntractor = getNoticeIntractor;
+    }
+
+    @Override
+    public void onDestroy() {
+        mainView = null;
+    }
+
+    @Override
+    public void onRefreshButtonClick() {
+
+        if (mainView != null) {
+            mainView.showProgress();
+        }
+        getNoticeIntractor.getNoticeArrayList(this);
+    }
+
+    @Override
+    public void requestDataFromServer() {
+        getNoticeIntractor.getNoticeArrayList(this);
+    }
+
+    @Override
+    public void onFinished(ArrayList<Notice> noticeArrayList) {
+        if (mainView != null) {
+            mainView.setDataToRecyclerView(noticeArrayList);
+            mainView.hideProgress();
+        }
+    }
+
+    @Override
+    public void onFailure(Throwable t) {
+        if (mainView != null) {
+            mainView.onResponseFailure(t);
+            mainView.hideProgress();
+        }
+    }
 }
