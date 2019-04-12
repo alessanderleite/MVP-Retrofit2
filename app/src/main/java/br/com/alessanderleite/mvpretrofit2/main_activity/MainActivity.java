cf49d55765be5c939622d9ba33b5b8b9db0c7791
alessanderleite <alessander.logistica@gmail.com>
2019-04-12 01:14:00 -0300

    code the MainActivity class to finalize the project

--- app/src/main/java/br/com/alessanderleite/mvpretrofit2/main_activity/MainActivity.java
+++ app/src/main/java/br/com/alessanderleite/mvpretrofit2/main_activity/MainActivity.java
@@ -2,14 +2,118 @@ package br.com.alessanderleite.mvpretrofit2.main_activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import br.com.alessanderleite.mvpretrofit2.R;
+import br.com.alessanderleite.mvpretrofit2.adapter.NoticeAdapter;
+import br.com.alessanderleite.mvpretrofit2.model.Notice;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainContract.MainView{
+
+    private ProgressBar progressBar;
+    private RecyclerView recyclerView;
+
+    private MainContract.presenter presenter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeToolbarAndRecyclerView();
+        initProgressBar();
+
+        presenter = new MainPresenterImpl(this, new GetNoticeIntractorImpl());
+        presenter.requestDataFromServer();
+    }
+
+    private void initializeToolbarAndRecyclerView() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        recyclerView = findViewById(R.id.recycler_view_employee_list);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+    }
+
+    private void initProgressBar() {
+        progressBar = new ProgressBar(this, null, android.R.attr.progressBarStyleLarge);
+        progressBar.setIndeterminate(true);
+
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+        relativeLayout.setGravity(Gravity.CENTER);
+        relativeLayout.addView(progressBar);
+
+        RelativeLayout.LayoutParams params = new
+                RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        progressBar.setVisibility(View.INVISIBLE);
+
+        this.addContentView(relativeLayout, params);
+    }
+
+    private RecyclerItemClickListener recyclerItemClickListener = new RecyclerItemClickListener() {
+        @Override
+        public void onItemClick(Notice notice) {
+            Toast.makeText(MainActivity.this,
+                    "List Title: " + notice.getTitle(),
+                    Toast.LENGTH_LONG).show();
+        }
+    };
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void setDataToRecyclerView(ArrayList<Notice> noticeArrayList) {
+
+        NoticeAdapter adapter = new NoticeAdapter(noticeArrayList, recyclerItemClickListener);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onResponseFailure(Throwable throwable) {
+        Toast.makeText(MainActivity.this,
+                "Something went wrong... Error message: " + throwable.getMessage(),
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        presenter.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_refresh) {
+            presenter.onRefreshButtonClick();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
